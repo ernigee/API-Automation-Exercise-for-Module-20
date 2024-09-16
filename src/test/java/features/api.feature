05693ebb00ff097e6f3a54b8ed49cb9c2fc01be7
Ernigee <erni.102299@gmail.com>
2024-09-16 12:52:24 +0700

    first commit

---
+++ src/test/java/features/api.feature
@@ -0,0 +1,4 @@
+Feature: Test Automation Rest API
+
+  Scenario: Test get list data normal
+    Given prepare URL valid for get list data
---
+++ src/test/java/stepDefinition/ApiStep.java
@@ -0,0 +1,10 @@
+package stepDefinition;
+
+import io.cucumber.java.en.Given;
+
+public class ApiStep {
+    @Given("prepare URL valid for get list data")
+    public void prepareURLValidForGetListData() {
+        System.out.println("Henlo Step");
+    }
+}
